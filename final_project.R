12857cb59b52da573ca48cfe3c9b029400bd05aa
aileenkuang <aileenk@uw.edu>
2023-04-25 08:56:25 -0700

    Add data sets to R file

---
+++ final_project.R
@@ -0,0 +1,12 @@
+# Aileen Kuang
+# Victoria Zhuang 
+
+built_units_df <- read.csv("Built_Units_Since_2010.csv")
+dem_2013_df <- read.csv("2013_B02001.csv")
+dem_2014_df <- read.csv("2013_B02001.csv")
+dem_2015_df <- read.csv("2013_B02001.csv")
+dem_2016_df <- read.csv("2013_B02001.csv")
+dem_2017_df <- read.csv("2013_B02001.csv")
+dem_2018_df <- read.csv("2013_B02001.csv")
+dem_2019_df <- read.csv("2013_B02001.csv")
+dem_2020_df <- read.csv("2013_B02001.csv")
