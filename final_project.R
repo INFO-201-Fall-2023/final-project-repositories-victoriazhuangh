632627498b4f76063fce5c7c23b95ca03fc510ab
aileenkuang <aileenk@uw.edu>
2023-05-02 00:10:26 -0700

    Name data frames and convert GEOID10 & GEOID20 in Built_Units_Since_2010 to same number type as demographic data frames

--- final_project.R
+++ final_project.R
@@ -5,35 +5,43 @@
 library(stringr)
 library(dplyr)
 
-built_units_df <- read.csv("Built_Units_Since_2010_Edited.csv")
-dem_2013_df <- read.csv("2013_B02001.csv")
-dem_2014_df <- read.csv("2014_B02001.csv")
-dem_2015_df <- read.csv("2015_B02001.csv")
-dem_2016_df <- read.csv("2016_B02001.csv")
-dem_2017_df <- read.csv("2017_B02001.csv")
-dem_2018_df <- read.csv("2018_B02001.csv")
-dem_2019_df <- read.csv("2019_B02001.csv")
-dem_2020_df <- read.csv("2020_B02001.csv")
+df_1 <- read.csv("Built_Units_Since_2010_Edited.csv")
+df_2 <- read.csv("2013_B02001.csv")
+df_3 <- read.csv("2014_B02001.csv")
+df_4 <- read.csv("2015_B02001.csv")
+df_5 <- read.csv("2016_B02001.csv")
+df_6 <- read.csv("2017_B02001.csv")
+df_7 <- read.csv("2018_B02001.csv")
+df_8 <- read.csv("2019_B02001.csv")
+df_9 <- read.csv("2020_B02001.csv")
 
 # Data Joining -----------------------------------------------------------------
 # You first need to to create a unified dataset (i.e. you need to join your 
 # datasets together). This means that the records in your two datasets need to 
 # be related some how, either by a shared key or a combination of fields.
 
-# Convert GEOID10 and GEOID20 in built_units_df from scientific notation to actual
-# numbers in Excel 
+# Name data frames
+built_units_df <- df_1
+dem_2013_df <- df_2
+dem_2014_df <- df_3
+dem_2015_df <- df_4
+dem_2016_df <- df_5
+dem_2017_df <- df_6
+dem_2018_df <- df_7
+dem_2019_df <- df_8
+dem_2020_df <- df_9
 
-format(built_units_df$GEOID10, scientific = FALSE)
+# Convert GEOID10 and GEOID20 in built_units_df from scientific notation to actual numbers 
+built_units_df$built_units_geo10 <- paste("1500000US", built_units_df$GEOID10, sep = "")
+built_units_df$built_units_geo20 <- paste("1500000US", built_units_df$GEOID20, sep = "")
 
-# First, aggregate dem_2020_df so that it can be joined
+# Aggregate dem_2020_df so that it can be joined
 
 # Aggregate built_units so that it can be joined 
 
-# Can join dem_2013_df to dem_2020_df by GEOID
+# Join all demographic data frames
 
-# Join dem_2020_df to joined dem_20XX_df by GEOID
-
-# Join dem_df to built_units by ??
+# Join demographic data frame to built units data frame 
 
 # Data Cleaning ----------------------------------------------------------------
 # Once you have created your joined dataset, you should then make sure your 
