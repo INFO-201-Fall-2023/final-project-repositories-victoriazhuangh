4df33a496a186785101f9fddc63f4576822d01a4
aileenkuang <aileenk@uw.edu>
2023-05-30 16:38:27 -0700

    add plot changes

--- data/plots.R
+++ data/plots.R
@@ -1,4 +1,5 @@
 source("final_project.R")
+source("map_income.r")
 
 library(plotly)
 
@@ -261,7 +262,137 @@ wall_line <- ggplot(wall_bu_df, aes(x = YEAR_FINAL, y = total, group = unit_stat
 wall_line + geom_text()
 wall_line <- ggplotly(wall_line, tooltip = "text")
 
-# Create grouped bar charts for income distribution in Chinatown ----
 
+# Create grouped bar charts for income distribution in Chinatown ----
+# Append income dfs for all years ----
+all_inc_df <- rbind(inc_2013, inc_2014, inc_2015, inc_2016, inc_2017, inc_2018, inc_2019)
+
+# Delete unnecessary columns ----
+all_inc_df <- all_inc_df[,c(1:20, 37:39)]
+
+# Create separate dfs for each separate percentage ----
+
+df_10k <- all_inc_df[,c(1:4, 20)]
+df_10k$income_bracket <- "10,000 or less"
+names(df_10k)[names(df_10k) == "less_than_10k"] <- "Count"
+
+df_10k_14.99k <- all_inc_df[,c(1:3, 5, 20)]
+df_10k_14.99k$income_bracket <- "10,000 to 14,999"
+names(df_10k_14.99k)[names(df_10k_14.99k) == "X10k_to_14.99k"] <- "Count"
+
+df_15k_19.99k <- all_inc_df[,c(1:3, 6, 20)]
+df_15k_19.99k$income_bracket <- "15,000 to 19,999"
+names(df_15k_19.99k)[names(df_15k_19.99k) == "X15k_to_19.99k"] <- "Count"
+
+df_20k_24.99k <- all_inc_df[,c(1:3, 7, 20)]
+df_20k_24.99k$income_bracket <- "20,000 to 24,999"
+names(df_20k_24.99k)[names(df_20k_24.99k) == "X20k_to_24.99k"] <- "Count"
+
+df_25k_29.99k <- all_inc_df[,c(1:3, 8, 20)]
+df_25k_29.99k$income_bracket <- "25,000 to 29,999"
+names(df_25k_29.99k)[names(df_25k_29.99k) == "X25k_to_29.99k"] <- "Count"
+
+df_30k_34.99k <- all_inc_df[,c(1:3, 9, 20)]
+df_30k_34.99k$income_bracket <- "30,000 to 34,999"
+names(df_30k_34.99k)[names(df_30k_34.99k) == "X30k_to_34.99k"] <- "Count"
+
+df_35k_39.99k <- all_inc_df[,c(1:3, 10, 20)]
+df_35k_39.99k$income_bracket <- "35,000 to 39,999"
+names(df_35k_39.99k)[names(df_35k_39.99k) == "X35k_to_39.99k"] <- "Count"
+
+df_40k_44.99k <- all_inc_df[,c(1:3, 11, 20)]
+df_40k_44.99k$income_bracket <- "40,000 to 44,999"
+names(df_40k_44.99k)[names(df_40k_44.99k) == "X40k_to_44.99k"] <- "Count"
+
+df_45k_49.99k <- all_inc_df[,c(1:3, 12, 20)]
+df_45k_49.99k$income_bracket <- "45,000 to 49,999"
+names(df_45k_49.99k)[names(df_45k_49.99k) == "X45k_to_49.99k"] <- "Count"
+
+df_50k_59.99k <- all_inc_df[,c(1:3, 13, 20)]
+df_50k_59.99k$income_bracket <- "50,000 to 59,999"
+names(df_50k_59.99k)[names(df_50k_59.99k) == "X50k_to_59.99k"] <- "Count"
+
+df_60k_74.99k <- all_inc_df[,c(1:3, 14, 20)]
+df_60k_74.99k$income_bracket <- "60,000 to 74,999"
+names(df_60k_74.99k)[names(df_60k_74.99k) == "X60k_to_74.99k"] <- "Count"
+
+df_75k_99.99k <- all_inc_df[,c(1:3, 15, 20)]
+df_75k_99.99k$income_bracket <- "75,000 to 99,999"
+names(df_75k_99.99k)[names(df_75k_99.99k) == "X75k_99.99k"] <- "Count"
+
+df_100k_124.99k <- all_inc_df[,c(1:3, 16, 20)]
+df_100k_124.99k$income_bracket <- "100,000 to 124,999"
+names(df_100k_124.99k)[names(df_100k_124.99k) == "X100k_to_124.99k"] <- "Count"
+
+df_125k_149.99k <- all_inc_df[,c(1:3, 17, 20)]
+df_125k_149.99k$income_bracket <- "125,000 to 149,999"
+names(df_125k_149.99k)[names(df_125k_149.99k) == "X125k_to_149.99k"] <- "Count"
+
+df_150k_199.99k <- all_inc_df[,c(1:3, 18, 20)]
+df_150k_199.99k$income_bracket <- "150,000 to 199,999"
+names(df_150k_199.99k)[names(df_150k_199.99k) == "X150k_to_199.99k"] <- "Count"
+
+df_200k <- all_inc_df[,c(1:3, 19, 20)]
+df_200k$income_bracket <- "200k or more"
+names(df_200k)[names(df_200k) == "X200k_or_more"] <- "Count"
+
+# Append again ----
+all_inc_df2 <- rbind(df_10k, df_10k_14.99k, df_15k_19.99k, df_20k_24.99k, df_25k_29.99k,
+                     df_30k_34.99k, df_35k_39.99k, df_40k_44.99k, df_45k_49.99k, 
+                     df_50k_59.99k, df_60k_74.99k, df_75k_99.99k, df_100k_124.99k, 
+                     df_125k_149.99k, df_150k_199.99k, df_200k)
+
+# Filter for CID -----
+filt_cid <- all_inc_df2$GEO_ID == "1500000US530330090001" |
+  all_inc_df2$GEO_ID == "1500000US530330090002" |
+  all_inc_df2$GEO_ID == "1500000US530330091001" |
+  all_inc_df2$GEO_ID == "1500000US530330091002"
+
+cid_inc_df <- all_inc_df2[filt_cid,]
+
+# Group and summarize by year and percent ----
+grp <- group_by(cid_inc_df, Year, income_bracket)
+cid_inc_df_2 <- summarize(grp, 
+                          total = sum(Count))
+
+
+
+
+# Create stacked bar charts for income distribution ----
+# more specifically, the lowest and highest income brackets 
+
+cid_inc_bar <- ggplot(df_10k[filt_cid,], aes(x = Year, y = Count)) +
+  geom_bar(stat="identity")
+
+cid_inc_bar_2 <- ggplot(df_200k[filt_cid,], aes(x = Year, y = Count)) +
+  geom_bar(stat="identity")
 
 # Create grouped bar charts for income distribution in Wallingford ----
+
+# Filter data for Wallingford ----
+
+filt_wall <- all_inc_df2$GEO_ID == "1500000US530330050001" |
+  all_inc_df2$GEO_ID == "1500000US530330050002" |
+  all_inc_df2$GEO_ID == "1500000US530330050003" |
+  all_inc_df2$GEO_ID == "1500000US530330051001" |
+  all_inc_df2$GEO_ID == "1500000US530330051002" |
+  all_inc_df2$GEO_ID == "1500000US530330051003" |
+  all_inc_df2$GEO_ID == "1500000US530330052001" |
+  all_inc_df2$GEO_ID == "1500000US530330052002" |
+  all_inc_df2$GEO_ID == "1500000US530330052003" |
+  all_inc_df2$GEO_ID == "1500000US530330052004" |
+  all_inc_df2$GEO_ID == "1500000US530330052005"
+
+# Group by and summarize ----
+
+wall_inc_10k_df <- df_10k[filt_wall,]
+wall_inc_200k_df <- df_200k
+
+grp_wall <- group_by(wall_inc_10k_df, Year, income_bracket)
+wall_inc_df_2 <- summarize(grp_wall,
+                           total = sum(Count))
+
+# Create stacked bar charts 
+
+wall_inc_bar <- ggplot(wall_inc_df_2, aes(x = Year, y = total)) +
+  geom_bar(stat = "identity")
